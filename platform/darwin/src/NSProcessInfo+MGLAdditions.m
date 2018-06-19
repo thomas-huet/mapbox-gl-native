fe9d0530226fd538a46400466ced66cdfd201ab3
Thomas HUET <git@thomash.fr>
2018-06-19 09:50:15 +0200

    Remove SDKs

--- platform/darwin/src/NSProcessInfo+MGLAdditions.m
+++
@@ -1,9 +0,0 @@
-#import "NSProcessInfo+MGLAdditions.h"
-
-@implementation NSProcessInfo (MGLAdditions)
-
-- (BOOL)mgl_isInterfaceBuilderDesignablesAgent {
-    return [self.processName hasPrefix:@"IBDesignablesAgent"];
-}
-
-@end
